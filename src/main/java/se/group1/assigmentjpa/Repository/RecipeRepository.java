5346151022b04307c7ebe8bec5982ed3143a5933
azadeh1984 <azadeh_n1984@yahoo.com>
2023-02-01 13:29:17 +0100

    creat Ingredient class & Measurement class

--- src/main/java/se/group1/assigmentjpa/Repository/RecipeRepository.java
+++ src/main/java/se/group1/assigmentjpa/Repository/RecipeRepository.java
@@ -2,7 +2,8 @@ package se.group1.assigmentjpa.Repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import se.group1.assigmentjpa.entity.Recipe;
 
 @Repository
-public interface RecipeRepository extends CrudRepository<> {
+public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
 }
