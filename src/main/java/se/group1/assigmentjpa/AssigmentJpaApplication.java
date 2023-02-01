de3f89838b5729e450c012cf53cf04d7724a3855
kerry jalal <khariy34@hotmail.com>
2023-02-01 09:46:11 +0100

    Initial commit

---
+++ src/main/java/se/group1/assigmentjpa/AssigmentJpaApplication.java
@@ -0,0 +1,13 @@
+package se.group1.assigmentjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssigmentJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/group1/assigmentjpa/AssigmentJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.group1.assigmentjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
