1a60dd509d1e5346ae0accf91add0a23f6725fec
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2023-02-01 18:32:50 +0100

    fixing the Error and to correct ID field

--- src/main/java/se/group1/assigmentjpa/entity/RecipeIngredient.java
+++ src/main/java/se/group1/assigmentjpa/entity/RecipeIngredient.java
@@ -1,56 +1,37 @@
 package se.group1.assigmentjpa.entity;
 
-<<<<<<< HEAD
 
-=======
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
->>>>>>> 0a75288e51287bbb7aaccfec7de7d039758d45a5
+
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
-<<<<<<< HEAD
-@Entity
 
-=======
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
->>>>>>> 0a75288e51287bbb7aaccfec7de7d039758d45a5
+
 public class RecipeIngredient {
 
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-<<<<<<< HEAD
     private String id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    private Ingredient ingredient;
 
-    private double amount;
-
-    // todo : private Measurement measurement
-
-    @ManyToOne(cascade = CascadeType.ALL)
-    private Recipe recipe;
-
-
-
-=======
-    private int id;
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     private Ingredient ingredient;
 
-    private  Double amount;
+    private Double amount;
 
     private Measurement measurement;
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     private Recipe recipe;
-
-
->>>>>>> 0a75288e51287bbb7aaccfec7de7d039758d45a5
 }
+
