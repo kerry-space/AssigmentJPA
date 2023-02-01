2877aa1c978b6045fa453f2064886d1a38932be9
kerry jalal <khariy34@hotmail.com>
2023-02-01 21:51:49 +0100

    resolve problem

--- src/main/java/se/group1/assigmentjpa/Repository/RecipeInstructionRepository.java
+++ src/main/java/se/group1/assigmentjpa/Repository/RecipeInstructionRepository.java
@@ -1,7 +1,10 @@
-package se.group1.assigmentjpa.Repository;
+package se.group1.assigmentjpa.repository;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.group1.assigmentjpa.entity.RecipeInstruction;
 
+@Repository
 public interface RecipeInstructionRepository extends CrudRepository<RecipeInstruction, Integer> {
 }
 
